3ff7e76f2a83b9fd3f4bca2ff1078e5a7e51c4a9
Ananda Pore <anandapore45@gmail.com>
2025-01-10 10:30:54 +0530

    sample commit

--- src/ui/components/header.jsx
+++ src/ui/components/header.jsx
@@ -2,21 +2,56 @@ import React from 'react'
 
 export const  Header = () => {
   return (
-    <div className="bg-gray-800 justify-evenly">
-    <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-evenly items-center h-16">
+    <div className="bg-gray-800">
+    <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-16">
+      {/* Logo */}
       <div>
-        <a href="/" className="text-white text-4xl font-bold">MyWebsite</a>
+        <a href="/" className="text-white text-4xl font-bold">
+          MyWebsite
+        </a>
       </div>
-      <div className="flex space-x-6 my-7 py-6">
-        <a  id='home' href="#home" className="text-white hover:text-yellow-200 underline underline-offset-2 text-2xl">Home</a>
-        <a  id='project' href="#about" className="text-white hover:text-yellow-200 underline underline-offset-2 text-2xl">Project</a>
-        <a  id='knowledge' href="#services" className="text-white hover:text-yellow-200 underline underline-offset-2 text-2xl">Knowledge</a>
-        <a  id='contact' href="#contact" className="text-white hover:text-yellow-200 underline underline-offset-2 text-2xl">Contact</a>
+  
+      {/* Desktop Navigation Links */}
+      <div className="hidden lg:flex items-center space-x-6">
+        <a
+          href="#home"
+          className="text-white hover:text-yellow-200 underline underline-offset-2 text-2xl"
+        >
+          Home
+        </a>
+        <a
+          href="#project"
+          className="text-white hover:text-yellow-200 underline underline-offset-2 text-2xl"
+        >
+          Project
+        </a>
+        <a
+          href="#knowledge"
+          className="text-white hover:text-yellow-200 underline underline-offset-2 text-2xl"
+        >
+          Knowledge
+        </a>
+        <a
+          href="#contact"
+          className="text-white hover:text-yellow-200 underline underline-offset-2 text-2xl"
+        >
+          Contact
+        </a>
+      </div>
+  
+      {/* Mobile Navigation Menu Icon */}
+      <div>
+      <img
+        src="https://tse4.mm.bing.net/th?id=OIP.XeN_Rxuvb0DzZFodgX-LnQAAAA&pid=Api&P=0&h=180"
+        alt="Mobile menu icon"
+        className="h-10 w-10 lg:hidden"
+      />
       </div>
     </nav>
-    </div>
-    
+  </div>
+  
+
   )
 }
-
+     
 
